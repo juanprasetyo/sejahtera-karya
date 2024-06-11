1774cf2b13b966ffddc7b93a12fb4b583d46bba9
Juan Prasetyo <juanprasetyo000@gmail.com>
2024-06-11 23:20:13 +0700

    feat(auth): add basic auth features

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,32 @@
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  theme: {
-    extend: {},
-  },
-  plugins: [
-    require("@tailwindcss/typography"),
-    require('daisyui'),
-  ],
-  daisyui: {
-    themes: ["light", "dark"],
-  },
-}
+    content: [
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './vendor/laravel/jetstream/**/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/views/**/*.js',
+    ],
 
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [
+        forms,
+        typography,
+        require('daisyui'),
+    ],
+
+    daisyui: {
+        themes: ["light"],
+    },
+};
